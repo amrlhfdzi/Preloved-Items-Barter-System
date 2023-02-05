d81b05f47eb2fbc206db4de2205936040af46926
amrlhfdzi <hafidziamirul8@gmail.com>
2023-02-05 21:12:13 +0800

    backup delete

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,3 +1,27 @@
+<div>
+<div wire:ignore.self class="modal fade" id="#deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Category Delete</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <form wire:submit.prevent="destroyCategory">
+      <div class="modal-body">
+        <h6> Are you sure you want to delete this data?</h6>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Yes. Delete</button>
+      </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+
+
 <div class="page-wrapper mdc-toolbar-fixed-adjust">
         <main class="content-wrapper">
         <div class="mdc-layout-grid">
@@ -35,7 +59,7 @@
                             <td>{{ $category->status == '1' ? 'Hidden':'Visible' }}</td>
                             <td>
                                 <a href="{{ url('category/'.$category->id.'/edit')}}" class="btn btn-success">Edit</a>  
-                                <a href="" class="btn btn-danger">Delete</a>
+                                <a href="#" wire.click="deleteCategory({{$category->id}})" data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
                         @endforeach
@@ -54,4 +78,5 @@
             </div>
         </div>
         </main>
+        </div>
         </div>
