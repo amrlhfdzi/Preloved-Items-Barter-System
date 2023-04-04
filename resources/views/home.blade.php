16a16dd72cb50622fb577d058c80a1a19edc5731
amrlhfdzi <hafidziamirul8@gmail.com>
2023-04-04 14:34:21 +0800

    filter product in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -99,6 +99,7 @@
       <div class="py-3 py-md-5 bg-light">
     <div class="container">
         <div class="row">
+         
             <div class="col-md-12">
                 <h4 class="mb-4">My Products</h4>
             </div>
--- resources/views/productsCategory.blade.php
+++ resources/views/productsCategory.blade.php
@@ -8,13 +8,13 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>romofyi</title>
+      <title>Swapup</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
       <!-- bootstrap css -->
       <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-      <!-- <link rel="stylesheet" href="{{asset('css/custom.css')}}"> -->
+      <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
       <!-- style css -->
       <link rel="stylesheet" href="{{asset('css/style.css')}}">
       <!-- Responsive-->
@@ -26,12 +26,245 @@
       <!-- Tweaks for older IEs-->
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css"> -->
       <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+      @livewireStyles
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      
+      <!-- end loader -->
+      <!-- header -->
+      @include("navbar");
+      <!-- end header inner -->
+      <!-- end header -->
+      <!-- banner -->
+      
+      <!-- end six_box section -->
+      <!-- project section -->
+      <!-- <div id="project" class="project"> -->
+      <div class="py-3 py-md-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="mb-4">My Products</h4>
+            </div>
 
-      <style>
-        /* Product Card */
+            <livewire:product.category :products="$products" :category="$category"/>
+
+
+            
+
+        </div>
+        
+    </div>
+</div>
+
+
+
+
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="{{asset('js/jquery.min.js')}}"></script>
+      <script src="{{asset('js/popper.min.js')}}"></script>
+      <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+      <script src="{{asset('js/jquery-3.0.0.min.js')}}"></script>
+      <!-- sidebar -->
+      <script src="{{asset('js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+      <script src="{{asset('js/custom.js')}}"></script>
+
+
+      <style type="text/css">
+
+         .w-5
+         {
+            display:none
+         }
+
+body{
+    background:#eee;    
+}
+.widget-author {
+  margin-bottom: 58px;
+}
+.author-card {
+  position: relative;
+  padding-bottom: 48px;
+  background-color: #fff;
+  box-shadow: 0 12px 20px 1px rgba(64, 64, 64, .09);
+}
+.author-card .author-card-cover {
+  position: relative;
+  width: 100%;
+  height: 100px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.author-card .author-card-cover::after {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  content: '';
+  opacity: 0.5;
+}
+.author-card .author-card-cover > .btn {
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  padding: 0 10px;
+}
+.author-card .author-card-profile {
+  display: table;
+  position: relative;
+  margin-top: -22px;
+  padding-right: 15px;
+  padding-bottom: 16px;
+  padding-left: 20px;
+  z-index: 5;
+}
+.author-card .author-card-profile .author-card-avatar, .author-card .author-card-profile .author-card-details {
+  display: table-cell;
+  vertical-align: middle;
+}
+.author-card .author-card-profile .author-card-avatar {
+  width: 85px;
+  border-radius: 50%;
+  box-shadow: 0 8px 20px 0 rgba(0, 0, 0, .15);
+  overflow: hidden;
+}
+.author-card .author-card-profile .author-card-avatar > img {
+  display: block;
+  width: 100%;
+}
+.author-card .author-card-profile .author-card-details {
+  padding-top: 20px;
+  padding-left: 15px;
+}
+.author-card .author-card-profile .author-card-name {
+  margin-bottom: 2px;
+  font-size: 14px;
+  font-weight: bold;
+}
+.author-card .author-card-profile .author-card-position {
+  display: block;
+  color: #8c8c8c;
+  font-size: 12px;
+  font-weight: 600;
+}
+.author-card .author-card-info {
+  margin-bottom: 0;
+  padding: 0 25px;
+  font-size: 13px;
+}
+.author-card .author-card-social-bar-wrap {
+  position: absolute;
+  bottom: -18px;
+  left: 0;
+  width: 100%;
+}
+.author-card .author-card-social-bar-wrap .author-card-social-bar {
+  display: table;
+  margin: auto;
+  background-color: #fff;
+  box-shadow: 0 12px 20px 1px rgba(64, 64, 64, .11);
+}
+.btn-style-1.btn-white {
+    background-color: #fff;
+}
+.list-group-item i {
+    display: inline-block;
+    margin-top: -1px;
+    margin-right: 8px;
+    font-size: 1.2em;
+    vertical-align: middle;
+}
+.mr-1, .mx-1 {
+    margin-right: .25rem !important;
+}
+
+.list-group-item.active:not(.disabled) {
+    border-color: #e7e7e7;
+    background: #fff;
+    color: #ac32e4;
+    cursor: default;
+    pointer-events: none;
+}
+.list-group-flush:last-child .list-group-item:last-child {
+    border-bottom: 0;
+}
+
+.list-group-flush .list-group-item {
+    border-right: 0 !important;
+    border-left: 0 !important;
+}
+
+.list-group-flush .list-group-item {
+    border-right: 0;
+    border-left: 0;
+    border-radius: 0;
+}
+.list-group-item.active {
+    z-index: 2;
+    color: #fff;
+    background-color: #007bff;
+    border-color: #007bff;
+}
+.list-group-item:last-child {
+    margin-bottom: 0;
+    border-bottom-right-radius: .25rem;
+    border-bottom-left-radius: .25rem;
+}
+a.list-group-item, .list-group-item-action {
+    color: #404040;
+    font-weight: 600;
+}
+.list-group-item {
+    padding-top: 16px;
+    padding-bottom: 16px;
+    -webkit-transition: all .3s;
+    transition: all .3s;
+    border: 1px solid #e7e7e7 !important;
+    border-radius: 0 !important;
+    color: #404040;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: .08em;
+    text-transform: uppercase;
+    text-decoration: none;
+}
+.list-group-item {
+    position: relative;
+    display: block;
+    padding: .75rem 1.25rem;
+    margin-bottom: -1px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.125);
+}
+.list-group-item.active:not(.disabled)::before {
+    background-color: #ac32e4;
+}
+
+.list-group-item::before {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 3px;
+    height: 100%;
+    background-color: transparent;
+    content: '';
+}
+
+/* Product Card */
+
+
 .product-card{
     background-color: #fff;
     border: 1px solid #ccc;
@@ -55,6 +288,8 @@
 }
 .product-card .product-card-img img{
     width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
 .product-card .product-card-body{
     padding: 10px 10px;
@@ -95,149 +330,12 @@
     font-size: 12px;
     margin-top: 10px;
 }
+
 /* Product Card End */
-      </style>
-      @livewireStyles
-   </head>
-   <!-- body -->
-   <body class="main-layout">
+
+</style>
       
-      <!-- end loader -->
-      <!-- header -->
-      <header>
-         <!-- header inner -->
-         <div class="header">
-            
-            <div class="header_midil">
-               <div class="container">
-                  <div class="row d_flex">
-                     <div class="col-md-4">
-                        <ul class="conta_icon d_none1">
-                           <li><a href="#"><img src="{{asset('images/email.png')}}" alt="#"/> demo@gmail.com</a> </li>
-                        </ul>
-                     </div>
-                     <div class="col-md-4">
-                        <a class="logo" href="#"><img src="{{asset('images/logo.png')}}" alt="#"/></a>
-                     </div>
-                     <div class="col-md-4">
-                        <ul class="right_icon d_none1">
-                           <li><a href="#"><img src="{{asset('images/shopping.png')}}" alt="#"/></a> </li>
-                           <a href="#" class="order">Order Now</a> 
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="header_bottom">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8">
-                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
-                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                           <span class="navbar-toggler-icon"></span>
-                           </button>
-                           <div class="collapse navbar-collapse" id="navbarsExample04">
-                              <ul class="navbar-nav mr-auto">
-                                 <li class="nav-item active">
-                                    <a class="nav-link" href="{{url('/redirect')}}">Home</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="about.html">About</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="products.html">Products</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="fashion.html">Fashion</a>
-                                 </li>
-                                 
 
-                                 <li class="nav-item">
-                                 @if (Route::has('login'))
-                
-                    @auth
-                    <li class="nav-item dropdown">
-                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" style="position:relative; padding-left:50px;">
-                     <img src="/uploads/avatars/{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:absolute; top:20px; left:10px; border-radius:50%">
-                        {{Auth::user()->name}} <span class="caret"></span>
-                     </a> 
-
-                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                     
-                        <a class="dropdown-item" href="{{ route('logout')}}"
-                        onclick="event.preventDefault();
-                                       document.getElementById('logout-form').submit();">
-                                       {{__('Logout')}}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout')}}" method="POST" style="display:none;">
-                        @csrf
-                        </form>
-
-                        <a class="dropdown-item" href="{{url('/profile')}}">
-                                       {{__('User Profile')}}
-                        </a>
-
-                        <a class="dropdown-item" href="{{url('/create')}}">
-                                       {{__('Upload Item')}}
-                        </a>
-
-
-                        
-                        </div>
-                     </li>
-                    @else
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
-
-                        @if (Route::has('register'))
-                        <li class="nav-item"> <a class="nav-link" href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
-                        @endif
-                    @endauth
-                
-            @endif
-            </li>
-                              </ul>
-                           </div>
-                        </nav>
-                     </div>
-                     <div class="col-md-4">
-                        <div class="search">
-                           <form action="/action_page.php">
-                              <input class="form_sea" type="text" placeholder="Search" name="search">
-                              <button type="submit" class="seach_icon"><i class="fa fa-search"></i></button>
-                           </form>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </header>
-
-      <div class="py-3 py-md-5 bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h4 class="mb-4">Our Products</h4>
-                </div>
-
-                <livewire:product.category :products="$products" :category="$category"/>
-
-                
-            </div>
-        </div>
-      </div>
-
-
-
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
-      @livewireScripts
    </body>
 </html>
+
